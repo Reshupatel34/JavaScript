f0607a2783e48ca330bbf89e9604d3e9ef9c4522
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-04-26 22:54:15 +0530

    naming convention of variables

--- variables/app.js
+++ variables/app.js
@@ -4,5 +4,17 @@
         let b="wasssssuppp"
         console.log(b)
         console.log(typeof(b))
+    //  variables cannot be starting from numbers
+    // like for ex
+    // its syntax error
+    // var 1="hola"
+    // console.log(1)
 
-        
+
+
+        // naming convention can start from special chars like _,$
+         let _=3
+         console.log(_)
+
+         var $="Wassup"
+         console.log($)
