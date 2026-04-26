1efb53682997f423b452cac6b9e6625d9e6d9251
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-04-26 23:23:54 +0530

    Redeclaration and reassignment of let var and const

--- variables/app.js
+++ variables/app.js
@@ -25,5 +25,55 @@
 
         //  reserverd keyword
 
-        let let=14
-        console.log(let)
+        // let let=14
+        // console.log(let)
+
+
+
+
+
+        // redeclarat and reassign
+
+
+        // redeclare
+        // only var
+
+        var name="reshu"
+
+        console.log(name)
+
+        var name="riya"
+
+        console.log(name)
+
+        // reassign
+        // let and const --> introduec in ES6
+
+        let x=14
+        console.log(x)
+
+        // let x=true  //redeclare X
+        console.log(x)
+
+        // the error is Uncaught SyntaxError: Identifier 'x' has already been declared
+
+        // re assign
+        x=true
+        console.log(x)
+
+
+        // neither reassing nor redeclare
+        // in case of const
+        const z=1
+        console.log(z)
+        const z=2
+        console.log(2)
+
+        // error 
+        // SyntaxError: Identifier 'z' has already been declared
+
+
+        // and cant reassing also
+        z=true
+        console.log(z)
+        
