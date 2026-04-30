930966bd274e79e83920d06f5fac69cc32de53df
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-04-30 23:42:56 +0530

    [Reshu] instead of using .env to hide the mongodb uri , we used config

--- Coach/config/Db.js
+++ Coach/config/Db.js
@@ -1,11 +1,15 @@
 import mongoose from 'mongoose';
+import debug from 'debug';
+import config from 'config'
+
+const dbgr = debug("development:mongoose");
 
  const connectDb = async(req ,res)=>{
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/Coach");
-        console.log("MongoDb connected!");
+        await mongoose.connect(`${config.get("MONGODB_URI")}/Coach`);
+       dbgr("MongoDb connected!");
     }catch(err){
-       console.log("Error connecting the database",err);
+       dbgr("Error connecting the database",err);
     }
 }
 export default connectDb;
